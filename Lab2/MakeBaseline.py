def3284d45235905f6fdc1649c540df44abbcb60
Jason Walker <jwalker107@gmail.com>
2024-09-24 12:37:32 -0500

    Removed portions of the script for exercises

--- Lab2/MakeBaseline.py
+++ Lab2/MakeBaseline.py
@@ -81,7 +81,9 @@ def create_baseline_component( name,
                                script_type="application/x-Fixlet-Windows-Shell", 
                                include_in_relevance="true"):
     """Creates a BaselineComponent from an existing Fixlet """
-      
+    
+    # A BaselineComponent is an XML element with the following example structure:
+    #
     # <BaselineComponent 
     #       Name="CustomFixlet1" 
     #       IncludeInRelevance="true" 
@@ -94,40 +96,19 @@ def create_baseline_component( name,
     #    <Relevance>true</Relevance>
     # </BaselineComponent>
 
-    
-    
-
+    # Create a new Element of type "BaselineComponent"
     component=ElementTree.Element("BaselineComponent")
+
     # TODO: Update the attributes and append the child elements for this fixlet
 
-    # component.set("Name", name)
-    # component.set("IncludeInRelevance", include_in_relevance)    
-    # component.set("SourceSiteURL", source_site_url)
-    # component.set("SourceID", str(source_id))
-    # component.set("ActionName", action_name)
+    # TODO: Create an Element of type "ActionScript", set attributes and values, and append it to the component    
     
-    # actionscript_element=ElementTree.Element("ActionScript")
-    # actionscript_element.set("MIMEType",script_type)
-    # actionscript_element.text=action_script
-    # component.append(actionscript_element)
+    # TODO: Create an Element of type "SuccessCriteria", set attributes and values, and append it to the component
+
+    # TODO: Create an Element of type "Relevance", set attributes and values, and append it to the component
     
-    # success_criteria_element=ElementTree.Element("SuccessCriteria")
-    # success_criteria_element.set("Option", success_criteria)
-    # if success_criteria_element=="CustomRelevance":
-    #     success_criteria_element.text="Relevance"
-    # component.append(success_criteria_element)
-    
-    # relevance_element=ElementTree.Element("Relevance")
-    # relevance_element.text=relevance
-    # component.append(relevance_element)
+    # Return the filled-out component
     return component
-
-def create_baseline_component_group(name=""):
-    """Creates a BaselineComponentGroup to hold multiple BaselineComponents"""
-    component_group=ElementTree.Element("BaselineComponentGroup")
-    component_group.set("Name", name)
-    return component_group
-
 
 
 response=test_login(bigfix_server, bes_username, bes_password, verify=verify)
@@ -161,9 +142,14 @@ response=run_query(url, data, bes_username, bes_password, verify)
 response_json=response.json()
 fixlet_list=response_json["result"]
 
+# Load the baseline XML template
 baseline_xml=ElementTree.fromstring(baseline_template)
-component_group=create_baseline_component_group(name="My Component Group")
 
+# Create  a BaselineComponentGroup element
+component_group=ElementTree.Element("BaselineComponentGroup")
+component_group.set("Name", "New Component Group")
+
+# Loop through the fixlets retrieved by the query, and create a BaselineComponent for each.
 for fixlet in fixlet_list:
     component=create_baseline_component( name=fixlet[0], 
                             source_site_url=fixlet[1], 
@@ -176,20 +162,16 @@ for fixlet in fixlet_list:
                             include_in_relevance=("true" if fixlet[8]=="Fixlet" else "false")
                             )
     # TODO - append the new component to the component_group
-    # component_group.append(component)
+
 
 # TODO - find the BaselineComponentCollection element in the baseline_xml and append the component_group to it
-# component_collection=baseline_xml.find(".//BaselineComponentCollection")
-# component_collection.append(component_group)
 
 modification_time=baseline_xml.find(".//MIMEField[Name='x-fixlet-modification-time']/Value")
 if modification_time is not None:
     modification_time.text=to_bes_time()
 
 # TODO - find the SourceReleaseDate element in the baseline_xml and set it to the current date
-# source_release_date=baseline_xml.find(".//SourceReleaseDate")
-# if source_release_date is not None:
-#     source_release_date.text=to_bes_date()
+
 
 response=requests.post(url=f'{bigfix_server}/api/baselines/{baseline_site}', data=ElementTree.tostring(baseline_xml), headers={'Content-Type': 'text/xml'}, auth=(bes_username, bes_password), verify=verify)
 if response.ok:
